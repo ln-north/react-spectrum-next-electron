2a5c1350bde199abec2ceecde6f2e1e27217f39d
ln-north <tkita@typeproject.com>
2021-11-18 17:26:54 +0900

    added react-spectrum with next-transpile-modules

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -2,6 +2,13 @@ import { useEffect } from 'react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import {
+  SSRProvider,
+  Provider,
+  defaultTheme,
+  Button,
+} from "@adobe/react-spectrum";
+
 const IndexPage = () => {
   useEffect(() => {
     // add a listener to 'message' channel
@@ -15,15 +22,13 @@ const IndexPage = () => {
   }
 
   return (
-    <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
-      <button onClick={onSayHiClick}>Say hi to electron</button>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
+    <SSRProvider>
+      <Provider theme={defaultTheme}>
+        <Button variant="cta" onPress={() => alert("Hey there!")}>
+          Hello React Spectrum!
+        </Button>
+      </Provider>
+    </SSRProvider>
   )
 }
 
